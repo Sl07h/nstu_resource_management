07a490e22f14c0d72763cbc14c90e9b0715184e5
Sl07h <Sl07h@users.noreply.github.com>
2019-05-18 23:21:12 +0700

    LAB 7 is done

--- 7/lab7.c
+++ 7/lab7.c
@@ -1,9 +1,3 @@
-// Вариант 5:
-// Программа моделирует работу примитивной СУБД, хранящей единственную таблицу в оперативной памяти.
-// Выполняя некоторые циклы работ, K порожденных процессов посредством очереди сообщений передают 
-// процессу номер строки и её содержимое, на которое нужно изменить хранящиеся в ней данные.
-// Родительский процесс выполняет указанную операцию и возвращает старое содержимое изменённой строки. 
-
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,120 +8,99 @@
 #include <sys/msg.h>
 #include <fcntl.h>
 #include <errno.h>
-#define LENGTH 1024
+#define LENGTH 64
 #define K 5
 
-struct {
-	int msg_type;
-	char msg_text[1024];
-} message;
+struct msg {
+	long mtype;
+	char mtext[LENGTH];
+};
+
 
 char table[K][LENGTH];
 
-/*
- *
- *
- */
-// int replaceLine(int i, char* s) {
 
-// 	if (0 <= i && i < LENGTH) {
-// 		char* result;
-// 		strcpy(result, table[i]);
-// 		if (strlen(s) + 1 <= LENGTH)
-// 		{
-// 			strcpy(table[i], s);
-// 			return 0;
-// 		}
-// 		else {
-// 			printf("Слишком длинная строка\n");
-// 			return -1;
-// 		}
-// 	}
-// 	else {
-// 		printf("Неправильный индекс таблицы\n");
-// 		return -2;
-// 	}
-// }
-
-
-char* replaceLine(int i, char* s) {
-	char result = (char*)malloc(LENGTH);
-	strcpy(result, table[i]);
-	strcpy(table[i], s);
-	return result;
+// Вывод таблицы на экран
+void printTable()
+{
+	printf("Таблица:\n");
+	for (size_t i = 0; i < K; i++)
+		printf("%d:\t%s\n", i, table[i]);
 }
 
 
-/*
- *
- * 
- */
-void printTable(){
-    printf("Таблица:\n");
-    for (size_t i = 0; i < K; i++)
-        printf("%d:\t%s\n",i,table[i]);    
-}
+// Замена строки по команде от процесса-потомка
+void replaceTableLine(char *s){
 
+	int i =0;
+	int number = 0;
+	int len = strlen(s);
+	while (isdigit(s[i]) && i<len)
+	{
+		number*=10;
+		number+=s[i] - '0';
+		i++;
+	}
+	strncpy(table[number], s+i, len - i);
+}
 
 
 int main()
 {
-	// Создаём очередб сообщений
+	struct msg message;
+	// Создаём очередь сообщений
 	int msgQueueID;
-	key_t key = ftok("lab7", 67);
-	if (msgQueueID = msgget(key, 0666| IPC_CREAT) == -1){
-		if (errno == EEXIST) 
-		{
-			//if queue create a long long time ago
-			msgQueueID = msgget(IPC_PRIVATE, 0); 
-		}
-		else 
-		{
-			printf("ERROR! can't create message queue. \n");
-			exit(1);
-		}
+	key_t key;
+	if (key = ftok("lab7", "a") == -1)
+	{
+		printf("ftok error\n");
+		exit(-1);
 	}
-	
 
-	// Инициализируем таблину начальными значениями
+	if ((msgQueueID = msgget(key, IPC_CREAT | 0666)) == -1)
+	{
+		printf("Ошибка создания очереди\n");
+		exit(-1);
+	}
+
+	// Инициализируем таблицу начальными значениями
 	for (size_t i = 0; i < K; i++)
 		sprintf(table[i], "Строка номер %d", i);
 
-    // Выводим исходную таблицу
-    printTable();
+	// Выводим исходную таблицу
+	printTable();
 
 	// Изменяем содержимое таблицы в процессах-потомках
 	for (size_t i = 0; i < K; i++)
 	{
-		if (fork() == 0) {
+		if (fork() == 0)
+		{
 			printf("Процесс %d(%d) изменяет строку %d\n", i, getpid(), i);
-			message.msg_type = 1;
-			sprintf(message.msg_text, "Новая строка номер %d", i);
-			printf("Before: %s\n", message.msg_text);
-			msgsnd(msgQueueID, &message, sizeof(message), 0);
+			message.mtype = 1;
+			sprintf(message.mtext, "%d Новая строка номер %d", i, i);
+			// Отправляем через очередь сообщений
+			if (msgsnd(msgQueueID, &message, sizeof(message.mtext), IPC_NOWAIT) < 0)
+			{
+				printf("Send error\n");
+				exit(-1);
+			}
 			exit(0);
 		}
 	}
 
-	sleep(5);
 	// Ожидаем завершение K процессов-потомков
-	int status,wpid;
+	int status, wpid;
 	while ((wpid = wait(&status)) > 0);
-    
+
 	for (size_t i = 0; i < K; i++)
 	{
-		int len = msgrcv(msgQueueID, &message, sizeof(message), 1, 0);
-		//printf("After: %s\n", message.msg_text);
-		
-		 printf("Строка %d до изменения: \'%s\'. Её длина %d\n", i, message.msg_text, len);
-        // replaceLine(i, message.msg_text);
-		// char oldLine = (char*)malloc(LENGTH);
-        // strcpy(oldLine, replaceLine(i, message.msg_text));
-        // printf("Строка до изменения: %s", oldLine);
+		// Получем сообщение из очереди
+		msgrcv(msgQueueID, &message, sizeof(message.mtext), 1, IPC_NOWAIT);
+		// Заменяем строку таблицы
+		replaceTableLine(message.mtext);
+		// Выводим модифицированную таблицу
+		printTable();
 	}
-
-    // Выводим модифицированную таблицу
-    printTable();
 
 	// Уничтожаем очередь сообщений
 	msgctl(msgQueueID, IPC_RMID, NULL);
